bcc9a21890a1eace0b918ab0ee64579cabdbc263
Gaby Pombo <gabypombo@air-de-gaby.home>
2020-11-09 10:52:36 +0100

    boucle while pour niveau 2

--- exo1.py
+++ exo1.py
@@ -1,18 +1,26 @@
 import random 
 
 print("Bienvenue dans le jeu!")
-print("Vous avez 5 jarres devant vous, veuillez en choisir une")
 
-#Choix du nombre aleatoire perdant
-snake = random.randint(1, 5)#nbre de 1 a 5 pr le hasard
+#compteur de clés
+keys = 0
 
-#input pour demander nbre au joueur
-nbre = int(input('numero choisi')) 
+#répéter les manches jusqu'a ce qu'il y ait 3 clés !!! identation, tt doit ê ds while sinon ça va pas
+while keys != 3:
+    print("Vous avez 5 jarres devant vous, veuillez en choisir une")
 
-print("vous avez choisi la jarre n°", nbre)
+    #Choix du nombre aleatoire perdant
+    snake = random.randint(1, 5)#nbre de 1 a 5 pr le hasard
 
-#verification 
-if nbre == snake: #perdu 
-    print("perdu ! Un serpent apparaît")
-else: 
-    print("Félicitations, vous obtenez une clé magique ! Le serpent était dans la jarre n°", snake)
+    #input pour demander nbre au joueur
+    nbre = int(input('numero choisi')) 
+
+    print("vous avez choisi la jarre n°", nbre)
+
+    #verification 
+    if nbre != snake: #Gagné 
+        print("Félicitations, vous obtenez une clé magique ! Le serpent était dans la jarre n°", snake)
+        keys += 1 
+    else: 
+        print("perdu ! Un serpent apparaît")
+        keys -= 1
